4cdbf447cea237fae90e1429a5b2af7200818d3b
Kieran R. Prasch <kieranprasch@gmail.com>
2019-02-07 20:29:17 -0700

    Use enviorment default python executable to trace solc binary installation path

--- solc/install.py
+++ solc/install.py
@@ -122,11 +122,7 @@ def get_base_install_path(identifier):
             'solc-{0}'.format(identifier),
         )
     else:
-        return os.path.expanduser(os.path.join(
-            '~',
-            '.py-solc',
-            'solc-{0}'.format(identifier),
-        ))
+        return abspath(dirname(dirname(sys.executable)))
 
 
 def get_repository_path(identifier):
